ba6e3ff19c7cdbe7a5d756fe60a367f21723e52b
Muchiri Mwangi <muchiri.mwangi.100.@gmail.com>
2024-07-03 23:10:35 +0300

    second change

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const section = document.getElementById("section");
 const input = document.getElementById("input");
+const body = document.querySelector(".body")
 
 input.addEventListener("change", ()=>{
     section.style.borderRadius = input.value;
     section.style.backgroundColor = input.value;
+    input.style.borderColor = input.value;
+    body.style.background = `linear-gradient(to right, #9dd0ffgi, ${input.value})`;
 })
